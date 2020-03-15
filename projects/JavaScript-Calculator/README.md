9b8d4fbdbd5221bec48a1f108ac77d7881f4cf53
crewszk <crewszk@outlook.com>
2020-03-15 19:57:57 -0400

    added JavaScript v0.2

--- projects/JavaScript-Calculator/README.md
+++ projects/JavaScript-Calculator/README.md
@@ -1,4 +1,4 @@
-# JavaScript Calculator v0.1
+# JavaScript Calculator v0.2
 
 Used as practice for manipulating HTML documents with JavaScript, its a simple calculator with interactable buttons a working calculating interface.
 It supports addition, subtraction, division, and multiplication. As well as parenthesis operations.
@@ -9,10 +9,23 @@ Work in progress.
 To-Do List:
 
 - Add a side button to extend the calculator to be a scientific calculator with more operands like exponents, logorithms, trig functions, etc.
-- Add for replacing the previous operator if mistakely clicked such as clicking * will replace the + in the equation 82-4+
-- Develop my own parser instead of using eval() function to support additional operands
+- Add input checking for above operators and functions
+- Add evaluation & infix > postfix conversion for above operators and functions
 
 Updates:
+
+v0.2 :
+- Made textfields readonly
+- Improved upon input checking for illogical input:
+  - When entering a left parenthesis after an operand, or an operand after a right parenthesis, the parser defaults to a multiplication operation between them
+  - Using string.includes() to look for the existance of a period instead of a boolean flag
+  - When cliking an operator without inputing an operand, if the last input was an operator, it will be replaced with the new operator
+- I opted out of making my own parser. Instead I:
+  - Keep track of the equation using an array, an index is an operand, operator, or a parenthesis
+  - Convert the array from infix to postfix
+  - Compute the postfix operation using a stack, and each sub-operation is computed safely with eval()
+- Removed eCount counter for equation array in place of push() pop() and shift() array methods
+- Added console.log() throughout for output checking
 
 v0.1 :
 - Removed bulky JS file for styling buttons on hover in favor of CSS styling with hover attribute
--- projects/JavaScript-Calculator/scripts/operations.js
+++ projects/JavaScript-Calculator/scripts/operations.js
@@ -5,14 +5,11 @@ let numList = document.querySelectorAll(".number");
 let operatorList = document.querySelectorAll(".operations");
 let parenthesisList = document.querySelectorAll(".parenthesis");
 
-let period = false;
 let remainder = false;
 let pCount = 0;
 let equation = [];
-let eCount = 0;
 let historyEquation = [];
 let historyResult = [];
-let historyCount = 0;
 
 for(e of numList) {
     e.addEventListener('click', function(event) {
@@ -38,11 +35,8 @@ function addNumber(num) {
         docInput.value = '';
         remainder = false;
     }    
-    if(num == '.' && period == true) {
+    if(num == '.' && docInput.value.includes('.')) {
         return;
-    }
-    else if(num == '.') {
-        period = true;
     }
     else if(num == '0' && docInput.value == '') {
         return;
@@ -56,22 +50,27 @@ function addOperator(operator) {
     if(remainder == true) {
         remainder = false;
         docEquation.value = '';
-        docInput.value = '';
     }
-    let last = docEquation.value.charAt(docEquation.value.length - 1);
-    if(docInput.value == '' && !(last >= '0' && last <= '9') && last != ')') {
-        return;
-    }
-    else {
-        if(docInput.value != '') {
-            equation[eCount++] = docInput.value;
-            docEquation.value += docInput.value;
+    let last = equation[equation.length - 1];
+    if(docInput.value == '' && isNaN(last) == true && last != ')') {
+        if(last != '(') {
+            equation.pop();
+            docEquation.value = docEquation.value.substring(0, docEquation.value.length - 1);
         }
-        equation[eCount++] = operator;
-        docEquation.value += operator;
-        docInput.value = '';
-        period = false;
+        else {
+            return;
+        }
     }
+    else if(docInput.value != '') {
+        if(last == ')') {
+            equation.push('*');
+        }
+        equation.push(docInput.value);
+        docEquation.value += docInput.value;
+    }
+    equation.push(operator);
+    docEquation.value += operator;
+    docInput.value = '';
 }
 
 function addParenthesis(parenthesis) {
@@ -80,31 +79,30 @@ function addParenthesis(parenthesis) {
         docEquation.value = '';
         docInput.value = '';
     }
-    let last = docEquation.value.charAt(docEquation.value.length - 1);
+    let last = equation[equation.length - 1];
     if(parenthesis == '(') {
         pCount++;
         if(docInput.value != '') {
-            equation[eCount++] = docInput.value;
+            equation.push(docInput.value);
             docEquation.value += docInput.value;
-            equation[eCount++] = '*';
-            docEquation.value += '*';
+            equation.push('*');
         }
-        equation[eCount++] = parenthesis;
+        equation.push(parenthesis);
         docEquation.value += parenthesis;
         docInput.value = '';
-        period = false;
+        
     }
     else {
         if(pCount > 0 && (last != '(' || docInput.value != '')) {
             pCount--;
             if(docInput.value != '') {
-                equation[eCount++] = docInput.value;
+                equation.push(docInput.value);
                 docEquation.value += docInput.value;
             }
-            equation[eCount++] = parenthesis;
+            equation.push(parenthesis);
             docEquation.value += parenthesis;
             docInput.value = '';    
-            period = false;        
+                    
         }
     }
 }
@@ -113,36 +111,119 @@ function clearInput() {
     docInput.value = '';
     docEquation.value = '';
     remainder = false;
-    period = false;
+    equation = [];
 }
 
 function backspace() {
+    if(remainder == true) {
+        return;
+    }
     docInput.value = docInput.value.substring(0, docInput.value.length - 1);
 }
 
 function addHistory() {
-    if(historyCount > 5) {
-        for(i = 1; i > 5; i++) {
-            historyEquation[i] = historyEquation[i - 1];
-            historyResult[i] = historyResult[i - 1];
-        }
-        historyCount = 4;
+    if(historyEquation.length > 5) {
+        historyEquation.shift();
+        historyResult.shift();
     }
-    historyEquation[historyCount] = docEquation.value;
-    historyResult[historyCount++] = docInput.value;
+    historyEquation.push(docEquation.value);
+    historyResult.push(docInput.value);
 }
 
 function evaluateEquation() {
     if(docInput.value != '') {
-        equation[eCount++] = docInput.value;
+        equation.push(docInput.value);
         docEquation.value += docInput.value;
         docInput.value = '';
     }
-    docInput.value = eval(docEquation.value);
-    addHistory();
+
+    console.log(`Equation Infix: ${equation}`)
+    equation = infixPostfixConversion();
+    console.log(`Equation Postfix: ${equation}`);
     docEquation.value += '=';
-    remainder = true;
-    period = false;
-    eCount = 0;
+    if(equation.length == 0) {
+        docInput.value = "INPUT ERROR";
+        remainder = false;
+    }
+    else {
+        let temp = [];
+        while(equation.length != 0) {
+            let current = equation[0];
+            if(isNaN(current) == false) {
+                temp.push(equation.shift())
+            }
+            else if(current == '*' || current == '/' || current == '-' || current == '+') {
+                let a = temp.pop();
+                let b = temp.pop();
+                temp.push(eval(`${b}${current}${a}`));
+                console.log(`${b} ${current} ${a} = ${temp[temp.length - 1]}`)
+                equation.shift();
+            }
+            else {
+                break;
+            }
+        }
+        console.log(`Final Answer: ${temp[0]}`)
+        docInput.value = temp.pop();
+        addHistory();
+    }
+
     equation = [];
+    remainder = true;
+        
+}
+
+function infixPostfixConversion() {
+    let postfixStack = ['('];
+    let ePostfix = [];
+    equation.push(')');
+
+    while(equation.length != 0) {
+        let current = equation[0];
+
+        if(current == '(') {
+            postfixStack.push(equation.shift());
+        }
+        else if(isNaN(current) == false) {
+            ePostfix.push(equation.shift());
+        }
+        else if(current == '*' || current == '/' || current == '-' || current == '+') {
+            while(precedence(current) <= precedence(postfixStack[postfixStack.length - 1])) {
+                ePostfix.push(postfixStack.pop());
+            }
+            postfixStack.push(equation.shift());
+        }
+        else if(current == ')') {
+            while(postfixStack[postfixStack.length - 1] != '(') {
+                ePostfix.push(postfixStack.pop());
+            }
+            postfixStack.pop();
+            equation.shift();
+        }
+        else {
+            ePostfix = [];
+            break;
+        }
+    }
+
+    return ePostfix;
+}
+
+function precedence(symbol) {
+    switch(symbol) {
+        case '^':
+            return 3;
+        case '*':
+        case '/':
+            return 2;
+        case '+':
+        case '-':
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+function toggleHistory() {
+    
 }
